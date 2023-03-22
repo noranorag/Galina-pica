65c74856069903992c7624ea2e76c66c13199206
Galiņa <nora.galina@sk.lvt.lv>
2023-03-22 14:27:50 +0200

    picas un cilveka objektiem pievienotas set metodes

--- src/cilveks.java
+++ src/cilveks.java
@@ -2,16 +2,33 @@
 public class cilveks {
 	String vards;
 	String uzvards;
-	int talrunis;
+	String talrunis;
 	String majasAdrese;
 	boolean atrak;
 	
 	//konstruktors
-	public cilveks(String vards, String uzvards, int talrunis, String majasAdrese) {
+	public cilveks(String vards, String uzvards, String talrunis, String majasAdrese) {
 		this.vards = vards;
 		this.uzvards = uzvards;
 		this.talrunis = talrunis;
 		this.majasAdrese = majasAdrese;
 	}
 	
+	//set metodes
+		public void setVards(String vards) {
+			this.vards=vards;
+		}
+		public void setUzvards(String uzvards) {
+			this.uzvards = uzvards;
+		}
+		public void setTalrunis(String talrunis) {
+			this.talrunis = talrunis;
+		}
+		public void setTalr(String majasAdrese) {
+			this.majasAdrese = majasAdrese;
+		}
+		public void setStudProg(boolean atrak) {
+			this.atrak = atrak;
+		}
+		
 }
--- src/pica.java
+++ src/pica.java
@@ -16,4 +16,25 @@ public class pica {
 		this.gurki = gurki;
 		this.izmers = izmers;
 	}
+	
+	//set metodes
+		public void setVeids(String veids) {
+			this.veids=veids;
+		}
+		public void setPeperoni(boolean peperoni) {
+			this.peperoni = peperoni;
+		}
+		public void setSiers(boolean siers) {
+			this.siers = siers;
+		}
+		public void setTomati(boolean tomati) {
+			this.tomati = tomati;
+		}
+		public void setStudProg(boolean gurki) {
+			this.gurki = gurki;
+		}
+		public void izmers(int izmers) {
+			this.izmers = izmers;
+		}
+		
 }
